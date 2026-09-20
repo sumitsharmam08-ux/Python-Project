220ed46dbf7174c449c7028784b9ec57165131c8
sumitsharmam08-ux <sumitsharma.m08@gmail.com>
2026-09-20 11:42:37 +0530

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.write('Hello, I am Sumit, Welcome to my quiz Zone.. Hope you like the game. You may please Proceed further for gaming...')
-age = 18
+age = st.number_input('Enter ur Age....')
 if age>= 18:
   st.write("You are eligible for License...")
 else:
