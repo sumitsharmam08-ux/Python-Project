80c7c9ca938fb231462eb1b57a2cceeeab42cc92
sumitsharmam08-ux <sumitsharma.m08@gmail.com>
2026-09-20 11:07:11 +0530

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -1,3 +1,3 @@
 import streamlit as st
 
-st.write('Hello, I am Sumit, Welcome to my quiz Zone.. Hope you like the game. You may please Proceed further for gaming...)
+st.write('Hello, I am Sumit, Welcome to my quiz Zone.. Hope you like the game. You may please Proceed further for gaming...')
